e7842f7b0a40ea8f84583b10985783e4ce4cfbba
Andrea Gomez <andreagh.918@gmail.com>
2023-01-18 14:36:03 +0100

    Inizio progetto

---
+++ src/main/java/com/system/exams/SystemExamsBApplication.java
@@ -0,0 +1,13 @@
+package com.system.exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemExamsBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemExamsBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/exams/SystemExamsBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemExamsBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
